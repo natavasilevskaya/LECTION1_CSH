e9402f5751c30a2e36b38dce753e391690627173
NataVasilevskaya <nata.vasilevskaya11@yandex.ru>
2023-01-16 21:55:55 +0300

    example6 file added

---
+++ example6_special/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Введите имя пользователя: "); 
+string username = Console.ReadLine(); // записываем в переменную имя пользователя
+
+if (username.ToLower() == "ната")
+{
+    Console.WriteLine("Ура, это же Ната!");
+}
+
+if (username.ToLower() == "михаил")
+{
+    Console.WriteLine("Ну ты и пидр!");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
