4b72124cf492d2d6ea2bac7690ae27feb0a09df9
NataVasilevskaya <nata.vasilevskaya11@yandex.ru>
2023-01-16 21:38:33 +0300

    example5 file added

---
+++ example5_rand/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа вычисляет сумму двух случайных чисел
+
+int numA = new Random() .Next(1,10);
+// 1 2 3 ... 9 new Random () - записывает число
+// .Next - задает диапозон генерируемых чисел(1,11)
+Console.WriteLine(numA);
+int numB = new Random() .Next(1,12);
+Console.WriteLine(numB);
+int result = numA + numB;
+Console.WriteLine(result);
