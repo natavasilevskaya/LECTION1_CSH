4cd98dcdc9ff4b5f4a953e929c37844050285dcb
NataVasilevskaya <nata.vasilevskaya11@yandex.ru>
2023-01-16 21:27:55 +0300

    example4 file added

---
+++ example4_Div/Program.cs
@@ -0,0 +1,8 @@
+﻿// Программа выполняет деление двух чисел
+
+double numberA = new Random(). Next (22, 94);
+Console.WriteLine(numberA);
+double numberB = new Random(). Next (22, 94);
+Console.WriteLine(numberB);
+double result = numberA / numberB;
+Console.WriteLine(result);
