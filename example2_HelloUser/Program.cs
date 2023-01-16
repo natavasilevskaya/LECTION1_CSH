d15cbcbc6fcd3b7ff07ebec26a219bc45a505658
NataVasilevskaya <nata.vasilevskaya11@yandex.ru>
2023-01-16 20:46:45 +0300

    example2 file added

---
+++ example2_HelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write ("Введите ваше имя"); // write - выводит в терминале текст в одну строку
+string username = Console.ReadLine (); 
+// создаем переменную - указываем ее тип, потом даем имя - юзернейм, = это значит - возьми то что справа и положи в то,что слева
+Console.Write("Привет, ");
+Console.Write(username);
+
+// или Console.Write("Привет, " + username);
